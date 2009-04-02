8d62a92cccd90748caf8b27fd7d4d375e3bbe55f
Martin Roth <mhroth@gmail.com>
2009-04-02 11:10:51 +0100

    Add AsioInitException.java

---
+++ src/com/synthbot/jasiohost/AsioInitException.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright 2009 Martin Roth (mhroth@gmail.com)
+ * 
+ *  This file is part of JAsioHost.
+ *
+ *  JVstHost is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JVstHost is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with JAsioHost.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.synthbot.jasiohost;
+
+public class AsioInitException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public AsioInitException(String message) {
+    super(message);
+  }
+	
+}
