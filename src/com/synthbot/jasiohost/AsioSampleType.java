2adbe424a2341a742a22b1126c8647eef2c0a189
Martin Roth <mhroth@gmail.com>
2009-04-22 14:33:06 +0100

    Clean up final references to JVstHost in code.
    Replaced with JAsioHost.

--- src/com/synthbot/jasiohost/AsioSampleType.java
+++ src/com/synthbot/jasiohost/AsioSampleType.java
@@ -3,12 +3,12 @@
  * 
  *  This file is part of JAsioHost.
  *
- *  JVstHost is free software: you can redistribute it and/or modify
+ *  JAsioHost is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  JVstHost is distributed in the hope that it will be useful,
+ *  JAsioHost is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
